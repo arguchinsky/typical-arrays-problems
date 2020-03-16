f8e37bdeff9845d46909447a7451d96fd639aebd
Arguchinsky Alexey <arguchinsky55@gmail.com>
2020-03-16 16:24:20 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (arguments[0] && array.length) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (arguments[0] && array.length) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (arguments[0] && array.length) ? array.reduce((acc, curr )=> acc += curr, 0) / array.length : 0;
 }
